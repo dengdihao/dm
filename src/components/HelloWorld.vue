f0d927486ff24714b13647c52f3f0793c6217565
dengdihao <dengdihao_888@163.com>
2018-08-13 08:46:50 +0800

    项目创建

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <button v-on:click="submits()"></button>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        data() {
+            return {
+                
+            }
+        },
+        methods:{
+            submits:function () {
+                this.$http.getHelloWorld()
+                    .then(function () {
+                        
+                    },function () {
+                        
+                    })
+            }
+        }
+    }
+</script>
+
+
+<style>
+
+
+</style>
